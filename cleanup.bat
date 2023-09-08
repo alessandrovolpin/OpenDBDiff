6db1eb4531e7d8cf71fe04248e31133a5263d9f9
Jed Spraul <jspraul@yahoo.com>
2023-09-07 22:49:40 -0400

    Add cleanup script

---
+++ cleanup.bat
@@ -0,0 +1,16 @@
+rmdir /q/s .\OpenDbDiff\bin
+rmdir /q/s .\OpenDbDiff\obj
+rmdir /q/s .\OpenDBDiff.Abstractions.Schema\bin
+rmdir /q/s .\OpenDBDiff.Abstractions.Schema\obj
+rmdir /q/s .\OpenDBDiff.Abstractions.Ui\bin
+rmdir /q/s .\OpenDBDiff.Abstractions.Ui\obj
+rmdir /q/s .\OpenDBDiff.CLI\bin
+rmdir /q/s .\OpenDBDiff.CLI\obj
+rmdir /q/s .\OpenDBDiff.SqlServer.Schema\bin
+rmdir /q/s .\OpenDBDiff.SqlServer.Schema\obj
+rmdir /q/s .\OpenDBDiff.SqlServer.Ui\bin
+rmdir /q/s .\OpenDBDiff.SqlServer.Ui\obj
+rmdir /q/s .\OpenDBDiff.Tests\bin
+rmdir /q/s .\OpenDBDiff.Tests\obj
+
+rmdir /q/s .vs
